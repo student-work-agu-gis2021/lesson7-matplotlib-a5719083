f2f2f7ea27f7da653bf143e758886e0a9805ed02
a5719083 <>
2021-05-30 14:08:07 +0000

    Create an new data frame called data

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -19,7 +19,9 @@ import numpy as np
 import pandas as pd
 
 # YOUR CODE HERE 1 to set data
-
+x = np.random.rand(1000)
+y = np.random.rand(1000)
+data = pd.DataFrame()
 # Check your random values
 print(data.head())
 
